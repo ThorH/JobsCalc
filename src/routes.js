7ab85cada899b30b8237d6ccff8cb6cb5bda8a8d
ThorH <thor.haubert@hotmail.com>
2021-08-27 18:14:52 -0300

    update value hour
    Update do valor da sua hora, baseado nos valores informados no profile

--- src/routes.js
+++ src/routes.js
@@ -20,12 +20,36 @@ const Profile = {
             return res.render(views + "profile", { profile: Profile.data })
         },
         update(req, res){
-             // req.body para pegar os dados
-             // definir quantas semnas tem um ano
-             // remover as semenas de férias do ano
-             // quantas horas por semana estou trabalhando
-             // total de horas trabalhadas no mes
+            // req.body para pegar os dados
+             const data = req.body
 
+            // definir quantas semnas tem um ano: 52
+             const weeksPerYear = 52
+            // remover as semenas de férias do ano, para pegar quantas semanas tem em 1 mes
+            const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12
+            // quantas horas por semana estou trabalhando na semana
+            const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
+            
+            // horas trabalhadas no mes
+            const monthlyTotalHours = weekTotalHours * weeksPerMonth
+
+            //qual será o valor da minha hora?
+            data["value-hour"] = data["monthly-budget"] / monthlyTotalHours
+
+            Profile.data = data
+
+            /* ou dava pra fazer assim:
+
+                const valueHour = data["monthly-budget"] / monthlyTotalHours
+                
+                Profile.data = {
+                    ...Profile.data,
+                    ...req.body,
+                    "value-hour": valueHour
+                }
+            */
+
+            return res.redirect('/profile')
         }
     }
 }
